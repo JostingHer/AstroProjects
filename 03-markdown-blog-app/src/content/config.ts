b17af43b085cf3d0151bd5fa5ac199b9ea57c51d
Jositngl <jostinjmhernandez@gmail.com>
2024-10-19 23:01:16 +0200

    cc

---
+++ 03-markdown-blog-app/src/content/config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: z.string(),
+    //   image: image().refine((img) => img.width < 1200, {
+    //     message: 'Image should be lower than 1200px',
+    //   }),
+
+      // Relación
+      author: z.string(),
+
+      // Relación
+      tags: z.array(z.string()),
+    }),
+});
+
+export const collections = {
+    // blog: la llave debe ser igual al nombre de la carpeta
+  blog: blogCollection,
+};
