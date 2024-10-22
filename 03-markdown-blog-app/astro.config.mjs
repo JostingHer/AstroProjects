4aa8de4129bde090d94bd66dad575aab6e741a2b
Jositngl <jostinjmhernandez@gmail.com>
2024-10-22 11:56:28 +0200

    subiendo url

--- 03-markdown-blog-app/astro.config.mjs
+++ 03-markdown-blog-app/astro.config.mjs
@@ -8,5 +8,5 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "https://example", // variable global
+  site: "https://jostingblogastro.netlify.app/", // variable global
 });
