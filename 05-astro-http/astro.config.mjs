667c330c573a28a80d213e01755498d5d80472f8
Jositngl <jostinjmhernandez@gmail.com>
2024-11-24 20:08:54 +0100

    asd

--- 05-astro-http/astro.config.mjs
+++ 05-astro-http/astro.config.mjs
@@ -1,22 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
+// import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
-// import node from '@astrojs/node';
- 
+
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig( {
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-
-  // adapter: node({
-  //   mode: 'standalone',
-  // }),
-  output: 'hybrid',
-
-  adapter: cloudflare(),
-});
+  integrations: [ mdx(), sitemap() ],
+  output: "hybrid",
+  adapter: cloudflare()
+} );
