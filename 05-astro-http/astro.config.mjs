32fc4340470d97e0cdca0d147ad8a8be1535d311
Jositngl <jostinjmhernandez@gmail.com>
2024-11-24 19:55:59 +0100

    toggle node by cloudflare

--- 05-astro-http/astro.config.mjs
+++ 05-astro-http/astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 // import node from '@astrojs/node';
-
+ 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
